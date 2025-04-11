f15a4d3d72c377707cbcb738c9cd014d277c3f95
Pedro Moraes <pedro.moraes@vsoft.com.br>
2025-04-11 12:08:40 -0300

    feat: add vue-router and add background color

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import './style.css'
 import App from './App.vue'
 import { getTenant } from '@/utils/tenant';
 import { tenantConfigs } from '@/configs/tenantConfigs';
+import router from '@/router/index'
+
+
 
 const tenant = getTenant();
 const config: any = tenantConfigs[tenant] as any || tenantConfigs['default'];
 
 // Aplica a cor no root
+document.documentElement.style.setProperty('--background-color', config.backgroundColor);
 document.documentElement.style.setProperty('--primary-color', config.primaryColor);
-createApp(App).mount('#app')
+const app =createApp(App);
+app.use(router)
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import List from '@/components/property/List.vue'
+</script>
+
+<template>
+  <List/>
+</template>
